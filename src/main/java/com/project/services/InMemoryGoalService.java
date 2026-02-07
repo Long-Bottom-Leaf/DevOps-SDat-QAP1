1cc9f05bb2a81ee33116d8cce8a30fb92decb4e8
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-02-07 17:59:14 -0330

    Built out InMemoryGoalService

--- src/main/java/com/project/services/InMemoryGoalService.java
+++ src/main/java/com/project/services/InMemoryGoalService.java
@@ -9,6 +9,51 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InMemoryGoalService {
+public class InMemoryGoalService implements GoalService {
 
+    private final ProgressService progressService;
+    private final List<Goal> goals = new ArrayList<>();
+
+    public InMemoryGoalService(ProgressService progressService) {
+        this.progressService = progressService;
+    }
+
+    // register goal
+        @Override
+        public void registerGoal(Goal goal) {
+            goals.add(goal);
+        }
+
+    // check if goal is met
+        @Override
+        public boolean isGoalMet(Goal goal) {
+
+            LocalDate end = LocalDate.now();
+            LocalDate start;
+
+            if (goal.getTimeframe() == GoalTimeframe.WEEKLY) {
+                start = end.minusDays(6);
+            } else {
+                start = end.minusMonths(1).plusDays(1);
+            }
+
+            WorkoutType type = goal.getWorkoutType();
+            int actualValue;
+
+            // delegate calculations to ProgressService
+            if (goal.getMetric() == GoalMetric.WORKOUT_COUNT) {
+                actualValue = progressService
+                        .totalWorkoutsByTypeAndDateRange(type, start, end);
+
+            } else if (goal.getMetric() == GoalMetric.TOTAL_DURATION) {
+                actualValue = progressService
+                        .totalDurationByTypeAndDateRange(type, start, end);
+
+            } else { // CALORIES
+                actualValue = progressService
+                        .totalCaloriesByTypeAndDateRange(type, start, end);
+            }
+
+            return actualValue >= goal.getTargetValue();
+        }
 }
